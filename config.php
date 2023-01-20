891291e955f20debea229f1615b3c2e2ee50d6cc
Kurnia34 <117438222+Kurnia34@users.noreply.github.com>
2023-01-20 20:55:58 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<!-- Reprogramed By Kurnia Padila -->
+<?php
+	$conn = new mysqli("localhost","root","","cart_system");
+	if($conn->connect_error){
+		die("Connection Failed!".$conn->connect_error);
+	}
+?>
